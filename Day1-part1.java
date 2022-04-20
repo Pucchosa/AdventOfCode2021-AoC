f647cd5407b77a283a1539dd5d237ae24b100b05
MissC <85432909+Pucchosa@users.noreply.github.com>
2022-04-20 11:17:01 +0200

    Create Day1-part1.java

---
+++ Day1-part1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Prog1 {
+	public static void main(String[] args) {
+		int ris=0;
+		int t=0;
+		ArrayList<Integer> lista=new ArrayList<Integer>();
+		Scanner sc=new Scanner(System.in);
+
+// Make List of input
+		while(true){
+			try{
+				int dep = sc.nextInt();
+				lista.add(dep);
+				}
+			catch(Exception e){
+				break;}
+		}
+		
+// make sum
+		for (int i=0;i<lista.size()-1;i++){
+			if(lista.get(i)<lista.get(i+1)){ris=ris+1;}
+		}
+		System.out.println("Result: "+ris);
+	}
+}
