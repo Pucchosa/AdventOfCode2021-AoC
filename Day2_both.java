58993a19195e45fef2b11e4eff8ce2619aedfabf
MissC <85432909+Pucchosa@users.noreply.github.com>
2022-04-20 11:22:18 +0200

    Update and rename Day2-part2 to Day2_both.java

---
+++ Day2_both.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Day2_both {
+	public static void main(String[] args) {
+		int dep=0;
+		int dis=0;
+		int aim=0;
+		int t=0;
+		Scanner sc=new Scanner(System.in);
+		String x="";
+		int val=0;
+		
+// calculate 
+		while(t==0){
+			x=sc.next();
+			val=sc.nextInt();
+			if (x.equals("up")){
+				aim=aim-val;
+			}
+			else if(x.equals("forward")){
+				dis=dis+val;
+				dep=dep+(val*aim);
+			}
+			else if(x.equals("down")){
+				aim=aim+val;
+			}
+			else if (x.equals("stop")){
+				t=3;
+				break;
+			}
+		}
+		
+		int res=dis*dep;
+		int ris=dis*aim;
+		System.out.println("risultato Part1: "+ris);
+		System.out.println("risultato Part2: "+res);
+	}
+}
