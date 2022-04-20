5d051da8560001b953e17fae08f9a923253fe64f
MissC <85432909+Pucchosa@users.noreply.github.com>
2022-04-20 11:18:05 +0200

    Update Day1-part2.java

--- Day1-part2.java
+++ Day1-part2.java
@@ -1,12 +1,13 @@
 import java.util.*;
 
-public class Program {
+public class Prog2 {
 	public static void main(String[] args) {
 		int ris=0;
 		int t=0;
 		ArrayList<Integer> lista=new ArrayList<Integer>();
 		Scanner sc=new Scanner(System.in);
 
+// make List of input
 		while(true){
 			try{
 				int dep = sc.nextInt();
@@ -14,7 +15,9 @@ public class Program {
 				}
 			catch(Exception e){
 				break;}
-			}
+		}
+		
+// make sum
 		for (int i=0;i<lista.size()-3;i++){
 			int A=lista.get(i)+lista.get(i+1)+lista.get(i+2);
 			int B=lista.get(i+1)+lista.get(i+2)+lista.get(i+3);
